698f5133432ffa7ba1d21ffe95cb37130695aa6d
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2017-03-05 10:56:09 +0100

    handle column less table/view/synonym access

--- database/utils/view/plscope_col_usage.sql
+++ database/utils/view/plscope_col_usage.sql
@@ -15,30 +15,89 @@
 */
 
 CREATE OR REPLACE VIEW plscope_col_usage AS
-SELECT ids.owner,
-       ids.object_type,
-       ids.object_name,
-       ids.line,
-       ids.col,
-       ids.procedure_name,
-       CASE 
-          WHEN refs.usage = 'EXECUTE' THEN
-             refs.type
-       END AS operation,
-       ids.ref_owner,
-       ids.ref_object_type,
-       ids.ref_object_name,
-       ids.name as column_name
-  FROM plscope_identifiers ids
-  JOIN plscope_identifiers refs
-    ON refs.usage_id = ids.usage_context_id
-       AND refs.owner = ids.owner
-       AND refs.object_type = ids.object_type
-       AND refs.object_name = ids.object_name
- WHERE ids.type = 'COLUMN'
- ORDER BY ids.owner,
-          ids.object_type,
-          ids.object_name,
-          ids.line,
-          ids.col,
-          ids.path_len;
+WITH
+   cols AS (
+      SELECT ids.owner,
+             ids.object_type,
+             ids.object_name,
+             ids.line,
+             ids.col,
+             ids.procedure_name,
+             CASE 
+                WHEN refs.usage = 'EXECUTE' THEN
+                   refs.type
+             END AS operation,
+             ids.ref_owner,
+             ids.ref_object_type,
+             ids.ref_object_name,
+             ids.name as column_name
+        FROM plscope_identifiers ids
+        JOIN plscope_identifiers refs
+          ON refs.usage_id = ids.usage_context_id
+             AND refs.owner = ids.owner
+             AND refs.object_type = ids.object_type
+             AND refs.object_name = ids.object_name
+       WHERE ids.type = 'COLUMN'
+   ),
+   missing_cols AS (
+      SELECT t.owner,
+             t.object_type,
+             t.object_name,
+             t.line,
+             t.col,
+             t.procedure_name,
+             t.operation,
+             coalesce(o.owner, t.ref_owner) AS ref_owner,
+             coalesce(o.object_type, t.ref_object_type) AS ref_object_type,
+             coalesce(o.object_name, t.ref_object_name) AS ref_object_name,
+             tc.column_name
+        FROM plscope_tab_usage t
+        LEFT JOIN dba_synonyms s
+          ON s.owner            = t.ref_owner 
+             AND s.synonym_name = t.ref_object_name
+        LEFT JOIN dba_objects o 
+          ON o.owner            = s.table_owner 
+            AND o.object_name   = s.table_name
+        LEFT JOIN cols c
+          ON t.owner                                        = c.owner
+             AND t.object_type                              = c.object_type
+             AND t.object_name                              = c.object_name
+             AND t.procedure_name                           = c.procedure_name
+             AND coalesce(o.owner, t.ref_owner)             = c.ref_owner
+             AND coalesce(o.object_type, t.ref_object_type) = c.ref_object_type
+             AND coalesce(o.object_name, t.ref_object_name) = c.ref_object_name
+        JOIN dba_tab_columns tc
+          ON tc.owner = t.owner
+             AND tc.table_name = coalesce(o.object_name,t.ref_object_name)
+       WHERE direct_dependency = 'YES' 
+         AND c.owner IS NULL
+         AND t.operation IN ('INSERT', 'SELECT')
+   )
+SELECT owner,
+       object_type,
+       object_name,
+       line,
+       col,
+       procedure_name,
+       operation,
+       ref_owner,
+       ref_object_type,
+       ref_object_name,
+       column_name,
+       'YES' AS direct_dependency
+  FROM cols
+UNION ALL
+SELECT owner,
+       object_type,
+       object_name,
+       line,
+       col,
+       procedure_name,
+       operation,
+       ref_owner,
+       ref_object_type,
+       ref_object_name,
+       column_name,
+       'NO' AS direct_dependency
+  FROM missing_cols
+ORDER BY 1, 2, 3, 4, 5, 8, 9, 10, 11;
