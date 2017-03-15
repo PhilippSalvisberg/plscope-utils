d00691a3452941fd443b172fdebf603ab2f77054
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2017-03-15 14:09:20 +0100

    added demo for select, insert, update, delete and merge statements with sys.utl_xml.parsequery

---
+++ database/demo/demo_scripts/03_demo_utl_xml_parsequery.sql
@@ -0,0 +1,58 @@
+-- 1. parse the select statement using sys.utl_xml.parsequery
+SELECT parse_util.parse_query(user, q'[
+         SELECT /*+ordered */ d.deptno, d.dname, SUM(e.sal + NVL(e.comm, 0)) AS sal
+           FROM dept d
+           LEFT JOIN (SELECT * FROM emp WHERE hiredate > DATE '1980-01-01') e
+             ON e.deptno = d.deptno
+          GROUP BY d.deptno, d.dname
+       ]') 
+  FROM dual;
+
+-- 2. parse the insert statement using sys.utl_xml.parsequery
+SELECT parse_util.parse_query(user, q'[
+         INSERT INTO deptsal (dept_no, dept_name, salary)
+         SELECT /*+ordered */ d.deptno, d.dname, SUM(e.sal + NVL(e.comm, 0)) AS sal
+           FROM dept d
+           LEFT JOIN (SELECT * FROM emp WHERE hiredate > DATE '1980-01-01') e
+             ON e.deptno = d.deptno
+          GROUP BY d.deptno, d.dname
+       ]') 
+  FROM dual; 
+
+-- 3. parse the update statement using sys.utl_xml.parsequery
+SELECT parse_util.parse_query(user, q'[
+         UPDATE deptsal
+            SET salary = salary + 10
+          WHERE dept_no = (SELECT deptno 
+                             FROM dept
+                            WHERE dname = 'SALES')
+       ]') 
+  FROM dual;
+ 
+-- 4. parse the delete statement using sys.util_xml_parsequery
+SELECT parse_util.parse_query(user, q'[
+         DELETE deptsal
+          WHERE dept_no = (SELECT deptno 
+                             FROM dept
+                            WHERE dname = 'SALES')
+       ]') 
+  FROM dual;
+ 
+-- 5. parse the merge statement using sys.util_xml_parsequery
+SELECT parse_util.parse_query(user, q'[
+          MERGE INTO deptsal t
+          USING (
+                  SELECT /*+ordered */ d.deptno, d.dname, SUM(e.sal + NVL(e.comm, 0)) AS sal
+                    FROM dept d
+                    LEFT JOIN (SELECT * FROM emp WHERE hiredate > DATE '1980-01-01') e
+                      ON e.deptno = d.deptno
+                   GROUP BY d.deptno, d.dname
+                ) s
+             ON (t.dept_no = s.deptno)
+          WHEN MATCHED THEN 
+             UPDATE SET t.salary = s.sal * 0.1
+          WHEN NOT MATCHED THEN
+             INSERT (t.dept_no, t.dept_name, t.salary)
+             VALUES (s.deptno, s.dname, s.sal * 0.1)
+       ]')
+  FROM dual;
