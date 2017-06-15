9ed1a3afaf1cff9a8eb6f69fb6ae72d15ae46688
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2017-06-15 16:21:12 +0200

    Resolves #3. Fix inconsistencies in PL/Scope hierarchies. Used nearest existing usage_id as usage_context_id.

--- database/utils/view/plscope_identifiers.sql
+++ database/utils/view/plscope_identifiers.sql
@@ -15,49 +15,74 @@
 */
 
 CREATE OR REPLACE VIEW plscope_identifiers AS
-WITH 
-   ids AS (
+WITH
+   base_ids AS (
       SELECT owner,
-             name, 
-             signature, 
-             type, 
-             object_name, 
-             object_type, 
-             usage, 
-             usage_id, 
-             line, 
-             col, 
+             name,
+             signature,
+             type,
+             object_name,
+             object_type,
+             usage,
+             usage_id,
+             line,
+             col,
              usage_context_id,
              origin_con_id
         FROM dba_identifiers
       UNION ALL
-      SELECT owner, 
+      SELECT owner,
              ':' || NVL(sql_id, type) AS name,  -- intermediate statement marker colon
-             signature, 
-             type, 
-             object_name, 
-             object_type, 
+             signature,
+             type,
+             object_name,
+             object_type,
              'EXECUTE' AS usage, -- new, artificial usage
-             usage_id, 
-             line, 
-             col, 
+             usage_id,
+             line,
+             col,
              usage_context_id,
-             origin_con_id            
+             origin_con_id
        FROM dba_statements
+   ),
+   ids AS (
+      SELECT owner,
+             name,
+             signature,
+             type,
+             object_name,
+             object_type,
+             usage,
+             usage_id,
+             line,
+             col,
+             coalesce(
+                least(
+                   usage_context_id,
+                   max(usage_id) over (
+                      PARTITION BY owner, object_name, object_type
+                      ORDER BY usage_id
+                      ROWS BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING
+                   )
+                ),
+                0
+             ) AS usage_context_id, -- fix broken hierarchies
+             origin_con_id
+        FROM base_ids
    ),
    tree AS (
        SELECT ids.owner,
               ids.object_type,
-              ids.object_name, 
-              ids.line, 
+              ids.object_name,
+              ids.line,
               ids.col,
               ids.name,
               sys_connect_by_path(ids.name, '/') AS name_path,
               level as path_len,
               ids.type,
-              ids.usage, 
-              ids.signature, 
-              ids.usage_id, 
+              ids.usage,
+              ids.signature,
+              ids.usage_id,
               ids.usage_context_id,
               ids.origin_con_id
          FROM ids
@@ -69,16 +94,16 @@ WITH
    )
  SELECT tree.owner,
         tree.object_type,
-        tree.object_name, 
-        tree.line, 
-        tree.col, 
+        tree.object_name,
+        tree.line,
+        tree.col,
         last_value (
-           CASE 
-              WHEN tree.type in ('PROCEDURE', 'FUNCTION') AND tree.path_len = 2  THEN 
-                 tree.name 
+           CASE
+              WHEN tree.type in ('PROCEDURE', 'FUNCTION') AND tree.path_len = 2  THEN
+                 tree.name
            END
         ) IGNORE NULLS OVER (
-           PARTITION BY tree.owner, tree.object_name, tree.object_type 
+           PARTITION BY tree.owner, tree.object_name, tree.object_type
            ORDER BY tree.line, tree.col, tree.path_len
         ) AS procedure_name,
         REPLACE(tree.name, ':', NULL) AS name, -- remove intermediate statement marker
@@ -99,37 +124,37 @@ WITH
               AND src.name = tree.object_name
               AND src.line = tree.line
         ) AS text,
-        CASE 
+        CASE
            WHEN tree.name_path LIKE '%:%' AND tree.usage != 'EXECUTE' THEN
               -- ensure that this is really a child of a statement
               last_value (
-                 CASE 
-                    WHEN tree.usage = 'EXECUTE' THEN 
-                       tree.type 
+                 CASE
+                    WHEN tree.usage = 'EXECUTE' THEN
+                       tree.type
                  END
               ) IGNORE NULLS OVER (
-                 PARTITION BY tree.owner, tree.object_name, tree.object_type 
+                 PARTITION BY tree.owner, tree.object_name, tree.object_type
                  ORDER BY tree.line, tree.col, tree.path_len
               )
         END AS parent_statement_type,
-        CASE 
+        CASE
            WHEN tree.name_path LIKE '%:%' AND tree.usage != 'EXECUTE' THEN
               -- ensure that this is really a child of a statement
               last_value (
-                 CASE 
-                    WHEN tree.usage = 'EXECUTE' THEN 
-                       tree.signature 
+                 CASE
+                    WHEN tree.usage = 'EXECUTE' THEN
+                       tree.signature
                  END
               ) IGNORE NULLS OVER (
-                 PARTITION BY tree.owner, tree.object_name, tree.object_type 
+                 PARTITION BY tree.owner, tree.object_name, tree.object_type
                  ORDER BY tree.line, tree.col, tree.path_len
               )
         END AS parent_statement_signature,
         tree.signature,
-        tree.usage_id, 
+        tree.usage_id,
         tree.usage_context_id,
         tree.origin_con_id
    FROM tree
-   LEFT JOIN dba_identifiers refs 
+   LEFT JOIN dba_identifiers refs
      ON refs.signature = tree.signature
         AND refs.usage = 'DECLARATION';
